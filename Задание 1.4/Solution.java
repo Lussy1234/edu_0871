e6f2a8e9ff92464c9b1c2499c2b7d5df6d8b6926
Lussy1234 <69851579+Lussy1234@users.noreply.github.com>
2020-08-19 00:53:08 +0300

    Create Solution.java

---
+++ Задание 1.4/Solution.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Solution {
+    public static int number = 25;
+
+    public static void main(String[] args) {
+        System.out.println("25");
+        Scanner scan = new Scanner(System.in);
+        int Number = scan.nextInt();
+        System.out.println("5*5");
+    }
+}
