6055d66c2fbe68afbb892f810d986ebca3007a82
Lussy1234 <69851579+Lussy1234@users.noreply.github.com>
2020-08-18 23:46:09 +0300

    Create main.java

---
+++ Task_1.2/main.java
@@ -0,0 +1,13 @@
+package com.company;
+public class Main {
+        public static void main (String[] args) {
+            int a,b;
+            a = 3;
+            b = 2;
+            System.out.print(b);
+            System.out.print(" плюс ");
+            System.out.print(a);
+            System.out.print(" равно ");
+            System.out.print(a + b);
+        }
+    }
