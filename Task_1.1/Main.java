289043ca74cd08abd20a6f36329c9d42714ab0dd
Lussy1234 <69851579+Lussy1234@users.noreply.github.com>
2020-08-18 23:08:43 +0300

    Create Main.java

---
+++ Task_1.1/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+       String name = "Lussy";
+        int age = 26;
+        String city = "Moscow";
+       System.out.println("Hello "+name+" - "+age+" лет." +city);
+	// write your code here
+    }
+}
