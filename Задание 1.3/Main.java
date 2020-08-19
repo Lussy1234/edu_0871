506004cc40effa051c2d2a4107d77938239a2ac5
Lussy1234 <69851579+Lussy1234@users.noreply.github.com>
2020-08-19 14:52:09 +0300

    Update Main.java

--- Задание 1.3/Main.java
+++ Задание 1.3/Main.java
@@ -1,5 +1,9 @@
+package com.company;
 public  class  Main {
-    public  static  void  main ( String [] args ) {
+    public static void main(String[] args) {
         int a = 5;
         int b = 8;
         String s = "Людмила";
+        System.out.println(+a+ +b+ "s");
+    }
+}
