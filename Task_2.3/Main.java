355e6b1f4e5452aea0fa9a862c41a8d955550e6e
Lussy1234 <69851579+Lussy1234@users.noreply.github.com>
2020-08-23 16:03:50 +0300

    Create Main.java

---
+++ Task_2.3/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] agrs) {
+        int i = 1;    
+        int a = 1;
+        while (i < 11) {
+            a = a * i;
+            i++;
+        }
+        System.out.println(a);
+    }
+}
