6bd5aa60e1eedb808444018baadeff4b26529ffe
Lussy1234 <69851579+Lussy1234@users.noreply.github.com>
2020-08-26 17:18:16 +0300

    Create Main.java

---
+++ Task2.6/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+public static void main(String[]agrs){
+        for(int i = 1; i < 10; i++){
+        for(int a = 1; a < 10; a++){
+        System.out.print(a * i + "  ");
+        }
+        System.out.println("");
+        }
+       }
+}
