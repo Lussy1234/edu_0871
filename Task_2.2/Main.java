e695ed577bee7160ce53374e97c265f6c68e0317
Lussy1234 <69851579+Lussy1234@users.noreply.github.com>
2020-09-02 22:49:10 +0300

    Update Main.java

--- Task_2.2/Main.java
+++ Task_2.2/Main.java
@@ -7,18 +7,17 @@ public class Main {
         System.out.println(min(-20, -10, -40, 40));
         System.out.println(min(-20, -10, -30, -40));
     }
-
     public static int min(int a, int b) {
-        if (a > b) return b;
-        else return a;
+        if (a<b) {
+            return a;
+        } else {
+            return b;
+        }
     }
 
     public static int min(int a, int b, int c, int d) {
-        int minAB = min(a, b);
-        int minCD = min(c, d);
-        int ABCD = min(minAB, minCD);
+        return min(min(a, b), min(c, d));
 
-        return minAB;
     }
 
 }
