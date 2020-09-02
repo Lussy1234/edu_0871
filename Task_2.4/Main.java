c1d8a332f5640fbcb7a542284ca22763d4945389
Lussy1234 <69851579+Lussy1234@users.noreply.github.com>
2020-09-02 23:36:20 +0300

    Update Main.java

--- Task_2.4/Main.java
+++ Task_2.4/Main.java
@@ -1,12 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        for (int a = 1; a <= 5; a++) {
-            int result = 0;
-            for (int a2 = 1; a2 <= a; a2++) {
-                result = result + a2;
-            }
-            System.out.println(result);
+        int sum = 0;
+        for (int i = 1; i <= 5; i++) {
+            sum += i;
+            System.out.println(sum);
         }
     }
 }
-
