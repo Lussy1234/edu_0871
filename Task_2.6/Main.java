57e605a34ab66065bf6762589ffa3aee4e3cabb4
Lussy1234 <69851579+Lussy1234@users.noreply.github.com>
2020-09-03 00:35:56 +0300

    Update Main.java

--- Task_2.6/Main.java
+++ Task_2.6/Main.java
@@ -1,10 +1,14 @@
 public class Main {
-public static void main(String[]agrs){
-        for(int i = 1; i < 10; i++){
-        for(int a = 1; a < 10; a++){
-        System.out.print(a * i + "  ");
+    public static void main(String[]args){
+        int i = 1, j = 1;
+        while(i <= 10) {
+            System.out.print(i + " ");
+            while(j <= 9){
+                j++;
+                System.out.print(i*j + " ");
+            }
+            i++;
+            j=1;
+            System.out.println("");
         }
-        System.out.println("");
-        }
-       }
-}
+    }
